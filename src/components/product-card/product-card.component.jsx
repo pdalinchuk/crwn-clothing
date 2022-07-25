05371882d8e98819f085a2deee20530a631818eb
pdalinchuk <pavel.dalinchuk@gmail.com>
2022-07-25 20:19:06 +0300

    creating cartItem component and add item functionality

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,16 @@
+import { useContext } from 'react';
+
 import Button from '../button/button.component';
+
+import { CartContext } from '../../contexts/cart.context';
 
 import './product-card.styles.scss';
 
 const ProductCard = ({ product }) => {
   const { name, imageUrl, price } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
 
   return (
     <div className='product-card-container'>
@@ -12,7 +19,9 @@ const ProductCard = ({ product }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
-      <Button buttonType='inverted'>Add to Card</Button>
+      <Button buttonType='inverted' onClick={addProductToCart}>
+        Add to Card
+      </Button>
     </div>
   );
 };
