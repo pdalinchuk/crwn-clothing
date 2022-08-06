e7066f8428d8f3f5194334cca1679fb94a336d2d
pdalinchuk <pavel.dalinchuk@gmail.com>
2022-08-06 17:52:26 +0300

    migrating cart-context over to redux

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,6 +1,10 @@
-import { useContext } from 'react';
-
-import { CartContext } from '../../contexts/cart.context';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  addItemToCart,
+  removeItemFromCart,
+  clearItemFromCart,
+} from '../../store/cart/cart.action';
+import { selectCartItems } from '../../store/cart/cart.selector';
 
 import {
   CheckoutItemContainer,
@@ -14,15 +18,16 @@ import {
 
 const CheckoutItem = ({ cartItem }) => {
   const { name, quantity, imageUrl, price } = cartItem;
+  const cartItems = useSelector(selectCartItems);
+  const dispatch = useDispatch();
 
-  const { addItemToCart, removeItemFromCart, clearItemFromCart } =
-    useContext(CartContext);
+  const addItemHandler = () => dispatch(addItemToCart(cartItems, cartItem));
 
-  const addItemHandler = () => addItemToCart(cartItem);
+  const removeItemHandler = () =>
+    dispatch(removeItemFromCart(cartItems, cartItem));
 
-  const removeItemHandler = () => removeItemFromCart(cartItem);
-
-  const clearItemHandler = () => clearItemFromCart(cartItem);
+  const clearItemHandler = () =>
+    dispatch(clearItemFromCart(cartItems, cartItem));
 
   return (
     <CheckoutItemContainer>
--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -1,17 +1,17 @@
 import { CATEGORIES_ACTIONS_TYPES } from './category.types';
 
 export const CATEGORIES_INITIAL_STATE = {
-  categoriesMap: {},
+  categories: [],
 };
 
 export const categoriesReducer = (state = CATEGORIES_INITIAL_STATE, action) => {
   const { type, payload } = action;
 
   switch (type) {
-    case CATEGORIES_ACTIONS_TYPES.SET_CATEGORIES_MAP:
+    case CATEGORIES_ACTIONS_TYPES.SET_CATEGORIES:
       return {
         ...state,
-        categoriesMap: payload,
+        categories: payload,
       };
     default:
       return state;
