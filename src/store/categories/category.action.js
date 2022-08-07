e4de6489a623a4c8abe9608956ab96bf1a4ee639
pdalinchuk <pavel.dalinchuk@gmail.com>
2022-08-07 20:28:02 +0300

    adding thunk pattern for fetching categories and using isLoading to render spinner

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,5 +1,26 @@
-import { createAction } from '../../utils/reducer/reducer.utils';
 import { CATEGORIES_ACTIONS_TYPES } from './category.types';
 
-export const setCategories = (categoriesArray) =>
-  createAction(CATEGORIES_ACTIONS_TYPES.SET_CATEGORIES, categoriesArray);
+import { createAction } from '../../utils/reducer/reducer.utils';
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+
+export const fetchCategoriesStart = () =>
+  createAction(CATEGORIES_ACTIONS_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categoriesArray) =>
+  createAction(
+    CATEGORIES_ACTIONS_TYPES.FETCH_CATEGORIES_SUCCESSED,
+    categoriesArray
+  );
+
+export const fetchCategoriesFailed = (error) =>
+  createAction(CATEGORIES_ACTIONS_TYPES.FETCH_CATEGORIES_FAILED, error);
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+  dispatch(fetchCategoriesStart());
+  try {
+    const categoriesArray = await getCategoriesAndDocuments('categories');
+    dispatch(fetchCategoriesSuccess(categoriesArray));
+  } catch (err) {
+    dispatch(fetchCategoriesFailed(err));
+  }
+};
